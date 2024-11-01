defae1a13f191dec0e882960944b12f42f10c96b
miguelvascon <vasconmiguel03@gmail.com>
2024-11-01 17:57:55 -0400

    Add test database for better organization

---
+++ db/test_schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS Game (
+    game_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    date TEXT NOT NULL,
+    result BOOLEAN DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Guesses (
+    game_id INTEGER NOT NULL,
+    guess_number INTEGER NOT NULL,
+    guess TEXT NOT NULL,
+    feedback TEXT,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (game_id, guess_number),
+    FOREIGN KEY (game_id) REFERENCES Game(game_id)
+);
